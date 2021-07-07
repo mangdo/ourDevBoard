d34fe316b400e696e41b8185de04626fc65d017e
mangdo <70243735+mangdo@users.noreply.github.com>
2021-07-07 12:37:15 +0900

    refactor : 기존페이지에 타임리프 적용하기
    기존 페이지에 템플릿엔진인 타임리프를 적용시켰다. static에는 css, js, image와 같은 정적파일만 존재하도록 변경하였다. 타임리프를 적용한 페이지들은 더이상 정적 파일이 아님으로 templates으로 이동시켰다.

--- src/main/java/com/example/ourdevboard/security/WebSecurityConfig.java
+++ src/main/java/com/example/ourdevboard/security/WebSecurityConfig.java
@@ -1,15 +1,15 @@
 package com.example.ourdevboard.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 가능하게 함
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,8 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 //.antMatchers("/detail*").hasAnyRole()
-                .antMatchers("/**").permitAll()
-                .anyRequest().permitAll()
+                .antMatchers("/", "/api/posts").permitAll()
+                .anyRequest().authenticated()
                 .and()
             .formLogin()
                 .loginPage("/user/login")
